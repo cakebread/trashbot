86863e5a3b63e39490ad95d2a7ddc2009cc98620
Rob Cakebread <cakebread@gmail.com>
2022-03-21 18:22:44 -0700

    Initial commit

---
+++ quasar/src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Trash Room
+        </q-toolbar-title>
+
+        <div>Reverend Dan K-XLU</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Essential Trash
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksData = [
+  {
+    title: 'Music For Nimrods',
+    caption: 'Reverend Dan Website',
+    link: 'http://musicfornimrods.net/'
+  },
+  {
+    title: 'Twitch',
+    caption: 'Rev Dan Live',
+    link: 'https://twitch.tv/reverenddankxlu'
+  },
+  {
+    title: 'Twitter',
+    caption: '@ReverendDanKXLU',
+    link: 'https://twitter.com/reverenddankxlu'
+  },
+  {
+    title: 'Instagram',
+    caption: 'Photos',
+    link: 'https://www.instagram.com/reverenddankxlu'
+  },
+  {
+    title: 'Facebook',
+    caption: 'Sucks',
+    link: 'https://facebook.com'
+  }
+];
+
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ quasar/src/pages/Index.vue
@@ -0,0 +1,69 @@
+<template>
+ <div class="q-pa-md q-gutter-sm">
+<q-btn label="Chat Log" color="primary" @click="basic = true" />
+    <q-btn label="Playlist" color="primary" @click="fixed = true" />
+
+    <q-dialog v-model="basic" transition-show="rotate" transition-hide="rotate">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Your Permanent Record</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+        </q-card-section>
+
+      </q-card>
+    </q-dialog>
+
+    <q-dialog v-model="fixed">
+      <q-card>
+        <q-card-section>
+          <div class="text-h6">Today's Playlist</div>
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-section style="max-height: 50vh" class="scroll">
+          <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+        </q-card-section>
+
+        <q-separator />
+
+      </q-card>
+    </q-dialog>
+<q-btn label="Listen" color="primary" @click="seamless = true" />
+
+    <q-dialog v-model="seamless" seamless position="bottom">
+      <q-card style="width: 350px">
+        <q-linear-progress :value="0.6" color="pink" />
+
+        <q-card-section class="row items-center no-wrap">
+          <div>
+            <div class="text-weight-bold">Land Shrimp and Long Pork</div>
+            <div class="text-grey">The Mormons</div>
+          </div>
+
+          <q-space />
+
+          <q-btn flat round icon="play_arrow" />
+          <q-btn flat round icon="pause" />
+          <q-btn flat round icon="close" v-close-popup />
+        </q-card-section>
+      </q-card>
+    </q-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      seamless: false,
+      basic: false,
+      fixed: false
+    }
+  }
+}
+</script>
